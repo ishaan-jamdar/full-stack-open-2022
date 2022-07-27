97353bb2a4d7662be4c835b7d5aeb4295f598bef
Ishaan Jamdar <ishaan.jamdar@gmail.com>
2022-07-27 18:58:10 -0400

    finished exercises 2.1-2.5

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,22 @@
+const Header = ({ course }) => <h2>{course}</h2>
+
+const Total = ({ sum }) => <strong>total of {sum} exercises</strong>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map(part => <Part key={part.id} part={part} />)}
+  </>
+
+const Course = ({ course }) =>
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    <Total sum={course.parts.reduce((sum, part) => sum + part.exercises, 0)} />
+  </div>
+
+export default Course;
